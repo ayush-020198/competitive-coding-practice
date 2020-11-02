d8d87b4dbd1b196cd630e9072e94c718232f07b3
ayush-020198 <ayush.shrivastavaujn@gmail.com>
2020-11-02 21:04:17 +0530

    3d-surface-area

---
+++ 3d-surface-area/main.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the surfaceArea function below.
+int surfaceArea(vector<vector<int>> A) {
+    int upper = A.size()*A[0].size();
+    int lower = A.size()*A[0].size();
+    
+    int sideone=0;
+
+    for(int i=0; i<A.size(); i++){
+        sideone+=A[i][0];
+        sideone+=A[i][A[0].size()-1];
+        for(int j=0; j<A[i].size()-1; j++){
+            sideone+=abs(A[i][j]-A[i][j+1]);
+        }
+    }
+    
+    int sidetwo=0;
+    for(int j=0; j<A[0].size(); j++){
+        sidetwo+=A[0][j];
+        sidetwo+=A[A.size()-1][j];
+        for(int i=0; i<A.size()-1; i++){
+            sidetwo+=abs(A[i][j]-A[i+1][j]);
+        }
+    }
+    
+    
+    int ans = upper+lower+sideone+sidetwo;
+    
+    return ans;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string HW_temp;
+    getline(cin, HW_temp);
+
+    vector<string> HW = split_string(HW_temp);
+
+    int H = stoi(HW[0]);
+
+    int W = stoi(HW[1]);
+
+    vector<vector<int>> A(H);
+    for (int i = 0; i < H; i++) {
+        A[i].resize(W);
+
+        for (int j = 0; j < W; j++) {
+            cin >> A[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int result = surfaceArea(A);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
